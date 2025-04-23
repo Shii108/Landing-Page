487115bb98094121982624c760e27042fcd84222
Shii108 <sujalpoudel01@gmail.com>
2025-04-23 12:37:50 +0545

    added some basic stuffs

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="border">
+    is this working
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/components/Layout.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <nav>
+      <div>
+        <div>Logo</div>
+        <p>Landwind</p>
+      </div>
+      <ul>
+        <li>Home</li>
+        <li>Company</li>
+        <li>Marketplace</li>
+        <li>Features</li>
+        <li>Team</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
