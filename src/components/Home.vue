25a12bff5854b63f9258be0b4c662db5cb679492
Shii108 <sujalpoudel01@gmail.com>
2025-04-24 12:33:46 +0545

    feat(home): section 3 added

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,10 +5,10 @@
         products & brands.</h1>
       <p>This free and open-source landing page template was built using the utility classes from Tailwind CSS and based
         on the components from the Flowbite Library and the Blocks System.</p>
-        <div>
-          <button class="border">View on Github</button>
-          <button class="border">Get Figma file</button>
-        </div>
+      <div>
+        <button class="border cursor-pointer">View on Github</button>
+        <button class="border cursor-pointer">Get Figma file</button>
+      </div>
     </div>
     <div>
       <div><img src="/hero.png" alt=""></div>
@@ -24,6 +24,38 @@
       <h2>Mashable</h2>
     </div>
   </section2>
+  <section3>
+    <div class="flex border">
+      <div>
+        <h2>Work with tools you already use</h2>
+        <p>Deliver great service experiences fast - without the complexity of traditional ITSM solutions. Accelerate
+          critical development work, eliminate toil, and deploy changes with ease</p>
+        <ul>
+          <li>Continuous integration and deployment</li>
+          <li>Development workflow</li>
+          <li>Knowledge management</li>
+        </ul>
+        <p>Deliver great service experiences fast - without the complexity of traditional ITSM solutions.</p>
+      </div>
+      <div><img src="/feature-1.png" alt="analytical graph"></div>
+    </div>
+    <div class="flex border">
+      <div><img src="/feature-2.png" alt="analytical graph"></div>
+      <div>
+        <h2>We invest in the world’s potential</h2>
+        <p>Deliver great service experiences fast - without the complexity of traditional ITSM solutions. Accelerate
+          critical development work, eliminate toil, and deploy changes with ease.</p>
+          <ul>
+            <li>Dynamic reports and dashboards</li>
+            <li>Templates for everyone</li>
+            <li>Development workflow</li>
+            <li>Limitless business automation</li>
+            <li>Knowledge management</li>
+          </ul>
+          <p>Deliver great service experiences fast - without the complexity of traditional ITSM solutions.</p>
+      </div>
+    </div>
+  </section3>
 </template>
 
 <script setup>
