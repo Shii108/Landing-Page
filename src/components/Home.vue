51eda919d01768347ac5d6d5c5cf2b16aee4b134
Shii108 <sujalpoudel01@gmail.com>
2025-04-24 16:52:33 +0545

    feat(home): final section added also icons and images added in corresponding sections

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -24,7 +24,7 @@
       <h2>Mashable</h2>
     </div>
   </section>
-  <section id="third-section">
+  <section id="third-section" class="bg-[#F9FAFB]">
     <div class="flex border">
       <div>
         <h2>Work with tools you already use</h2>
@@ -45,46 +45,57 @@
         <h2>We invest in the world's potential</h2>
         <p>Deliver great service experiences fast - without the complexity of traditional ITSM solutions. Accelerate
           critical development work, eliminate toil, and deploy changes with ease.</p>
-          <ul>
-            <li>Dynamic reports and dashboards</li>
-            <li>Templates for everyone</li>
-            <li>Development workflow</li>
-            <li>Limitless business automation</li>
-            <li>Knowledge management</li>
-          </ul>
-          <p>Deliver great service experiences fast - without the complexity of traditional ITSM solutions.</p>
+        <ul>
+          <li>Dynamic reports and dashboards</li>
+          <li>Templates for everyone</li>
+          <li>Development workflow</li>
+          <li>Limitless business automation</li>
+          <li>Knowledge management</li>
+        </ul>
+        <p>Deliver great service experiences fast - without the complexity of traditional ITSM solutions.</p>
       </div>
     </div>
   </section>
   <section id="fourth-section" class="flex border">
-    <div class="border">
+    <div class="border flex-1/2">
       <p>Trusted Worldwide</p>
       <h2>Trusted by over 600 million users and 10,000 teams</h2>
-      <p>Our rigorous security and compliance standards are at the heart of all we do. We work tirelessly to protect you and your customers.</p>
+      <p>Our rigorous security and compliance standards are at the heart of all we do. We work tirelessly to protect you
+        and your customers.</p>
       <a href="www.google.com">Explore Legality Guide</a>
       <a href="www.google.com">Visit the Trust Center</a>
     </div>
     <div class="border-3 border-red-900 grid grid-cols-2 ">
       <div class="border">
-        <div>here will be image</div>
+        <div><img src="/server.png" alt="server icon" class="w-10 h-10"></div>
         <h2>99.99% uptime</h2>
         <p>For Landwind, with zero maintenance downtime</p>
       </div>
       <div class="border">
-        <div>here will be image</div>
+        <div><img src="/persons.png" alt="two people icon" class="w-10 h-10"></div>
         <h2>600M+ Users</h2>
         <p>Trusted by over 600 milion users around the world</p>
       </div>
       <div class="border">
-        <div>here will be image</div>
+        <div><img src="/earth.png" alt="earth logo" class="w-10 h-10"></div>
         <h2>100+ countries</h2>
         <p>Have used Landwind to create functional websites</p>
       </div>
       <div class="border">
-        <div>here will be image</div>
+        <div><img src="/cart.png" alt="shopping cart" class="w-10 h-10"></div>
         <h2>5+ Million</h2>
         <p>FTransactions per day</p>
       </div>
+    </div>
+  </section>
+  <section id="fifth-section" class="border bg-[#F9FAFB]">
+    <div><img src="/PhQuotesFill (1).png" alt="quotes" class="w-10 m-auto"></div>
+    <p>"Landwind is just awesome. It contains tons of predesigned components and pages starting from login screen to
+      complex dashboard. Perfect choice for your next SaaS application."</p>
+    <div class="flex justify-center items-center border ">
+      <div><img src="/Favicon.png" alt="image of a girl" class="w-7"></div>
+      <p>Micheal Gough</p>
+      <p>CEO at Google</p>
     </div>
   </section>
 </template>
