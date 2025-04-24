a118274d4aeabe4eb68a26328c80d65727b75826
Shii108 <sujalpoudel01@gmail.com>
2025-04-24 12:08:08 +0545

    feat(home) : section 1 and section 2 were added

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,13 +1,33 @@
 <template>
-  <div>
-    <h1>Work in progress</h1>
-  </div>
+  <section1 class="flex border">
+    <div>
+      <h1>Building digital
+        products & brands.</h1>
+      <p>This free and open-source landing page template was built using the utility classes from Tailwind CSS and based
+        on the components from the Flowbite Library and the Blocks System.</p>
+        <div>
+          <button class="border">View on Github</button>
+          <button class="border">Get Figma file</button>
+        </div>
+    </div>
+    <div>
+      <div><img src="/hero.png" alt=""></div>
+    </div>
+  </section1>
+  <section2>
+    <div class="flex justify-around border">
+      <h2>airbnb</h2>
+      <h2>Google</h2>
+      <h2>Microsoft</h2>
+      <h2>Spotify</h2>
+      <h2>mailchimp</h2>
+      <h2>Mashable</h2>
+    </div>
+  </section2>
 </template>
 
 <script setup>
 
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -5,15 +5,15 @@
       <p class="font-semibold text-xl">Cappuccina</p>
     </div>
     <ul class="flex items-center gap-10 font-medium text-gray-700">
-      <router-link class="hover:text-red-800" to="/">Home</router-link>
-      <router-link class="hover:text-red-800" to="company">Company</router-link>
-      <router-link class="hover:text-red-800" to="marketplace">Marketplace</router-link>
-      <router-link class="hover:text-red-800" to="features">Features</router-link>
-      <router-link class="hover:text-red-800" to="team">Team</router-link>
-      <router-link class="hover:text-red-800" to="contact">Contact</router-link>
+      <router-link class="hover:text-[#5521B5]" to="/">Home</router-link>
+      <router-link class="hover:text-[#5521B5]" to="company">Company</router-link>
+      <router-link class="hover:text-[#5521B5]" to="marketplace">Marketplace</router-link>
+      <router-link class="hover:text-[#5521B5]" to="features">Features</router-link>
+      <router-link class="hover:text-[#5521B5]" to="team">Team</router-link>
+      <router-link class="hover:text-[#5521B5]" to="contact">Contact</router-link>
     </ul>
     <div>
-      <button class="border bg-blue-500 text-white px-4 py-2 rounded-md font-semibold">Download</button>
+      <button class="border bg-[#5521B5] text-white px-4 py-2 rounded-md font-semibold">Download</button>
     </div>
   </nav>
   <router-view></router-view>
