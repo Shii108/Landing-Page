10ef1e9894b45c217eee4d57da4f77c2887f78af
Shii108 <sujalpoudel01@gmail.com>
2025-04-28 11:38:09 +0545

    feat(home): added padding and margins in different sections

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,43 +1,47 @@
 <template>
   <section id="first-section" class="flex border">
-    <div>
+    <div class="border border-red-900 flex flex-col gap-6 justify-center">
       <h1 class="text-6xl font-extrabold tracking-wide text-gray-900">Building digital
         products & brands.</h1>
       <p class="text-xl text-gray-500 font-light">This free and open-source landing page template was built using the
         utility classes from Tailwind CSS and based
         on the components from the Flowbite Library and the Blocks System.</p>
-      <div>
-        <button class="border cursor-pointer text-sm px-5 py-3">View on Github</button>
-        <button class="border cursor-pointer text-sm px-5 py-3">Get Figma file</button>
+      <div class="flex gap-6">
+        <button
+          class="border border-gray-200 cursor-pointer text-sm px-5 py-3 font-medium rounded-md bg-[#F3F4F6] hover:bg-[#d6ddeb]">View
+          on Github</button>
+        <button
+          class="border border-gray-200 cursor-pointer text-sm px-5 py-3 font-medium rounded-md bg-[#F3F4F6] hover:bg-[#d6ddeb]">Get
+          Figma file</button>
       </div>
     </div>
     <div>
-      <div><img src="/hero.png" alt=""></div>
+      <div><img src="/hero.png" alt="image of a arrow in a computer"></div>
     </div>
   </section>
 
 
   <section id="second-section">
-    <div class="flex justify-around border">
-      <h2>airbnb</h2>
-      <h2>Google</h2>
-      <h2>Microsoft</h2>
-      <h2>Spotify</h2>
-      <h2>mailchimp</h2>
-      <h2>Mashable</h2>
+    <div class="flex justify-between border py-8">
+      <h2 class="text-3xl text-gray-400  hover:text-gray-900">airbnb</h2>
+      <h2 class="text-3xl text-gray-400  hover:text-gray-900">Google</h2>
+      <h2 class="text-3xl text-gray-400  hover:text-gray-900">Microsoft</h2>
+      <h2 class="text-3xl text-gray-400  hover:text-gray-900">Spotify</h2>
+      <h2 class="text-3xl text-gray-400  hover:text-gray-900">mailchimp</h2>
+      <h2 class="text-3xl text-gray-400  hover:text-gray-900">Mashable</h2>
     </div>
   </section>
 
 
   <section id="third-section" class="bg-[#F9FAFB]">
-    <div class="flex border">
-      <div>
+    <div class="flex border-3 border-green-800">
+      <div class="flex flex-col justify-between py-4 ">
         <h2 class="text-3xl font-extrabold text-gray-900 tracking-wide">Work with tools you already use</h2>
         <p class="text-xl font-light text-gray-500">Deliver great service experiences fast - without the complexity of
           traditional
           ITSM solutions. Accelerate
           critical development work, eliminate toil, and deploy changes with ease</p>
-        <ul class="border-t border-gray-200">
+        <ul class="flex flex-col gap-6 text-lg">
           <li class="flex gap-1 items-center font-medium text-gray-900"><span><img src="/checked.png" alt="checkmark"
                 class="w-5 h-5"></span><span>Continuous integration and deployment</span></li>
           <li class="flex gap-1 items-center font-medium text-gray-90"><span><img src="/checked.png" alt="checkmark"
@@ -49,13 +53,14 @@
           traditional
           ITSM solutions.</p>
       </div>
-      <div><img src="/feature-1.png" alt="analytical graph"></div>
+      <div class="border"><img src="/feature-1.png" alt="analytical graph"></div>
     </div>
     <div class="flex border">
       <div><img src="/feature-2.png" alt="analytical graph"></div>
       <div class="border border-pink-500 flex flex-col">
         <h2 class="text-3xl font-extrabold">We invest in the world's potential</h2>
-        <p class="font-light text-xl text-gray-500">Deliver great service experiences fast - without the complexity of traditional
+        <p class="font-light text-xl text-gray-500">Deliver great service experiences fast - without the complexity of
+          traditional
           ITSM solutions. Accelerate
           critical development work, eliminate toil, and deploy changes with ease.</p>
         <ul class="border">
@@ -80,58 +85,61 @@
             <span>Knowledge management</span>
           </li>
         </ul>
-        <p class="font-light text-xl text-gray-400 ">Deliver great service experiences fast - without the complexity of traditional
+        <p class="font-light text-xl text-gray-400 ">Deliver great service experiences fast - without the complexity of
+          traditional
           ITSM solutions.</p>
       </div>
     </div>
   </section>
 
 
-  <section id="fourth-section" class="flex border">
-    <div class="border flex-1/2">
+  <section id="fourth-section" class="flex border py-[96px]">
+    <div class="border-3 border-purple-900 flex-1/2 flex flex-col gap-4">
       <p class="font-medium text-lg text-purple-600">Trusted Worldwide</p>
       <h2 class="text-3xl font-extrabold text-gray-900">Trusted by over 600 million users and 10,000 teams</h2>
-      <p class="text-gray-500 font-light text-[20px]">Our rigorous security and compliance standards are at the heart of
+      <p class="text-gray-500 font-light text-xl border-t pt-4">Our rigorous security and compliance standards are at the heart of
         all we do. We work tirelessly to protect you
         and your customers.</p>
-      <a href="www.google.com" class="font-medium text-purple-600 block">Explore Legality Guide</a>
-      <a href="www.google.com" class="font-medium text-purple-600">Visit the Trust Center</a>
+      <a href="www.google.com" class="font-medium text-purple-600 block">Explore Legality Guide <span class="text-2xl">→</span></a>
+      <a href="www.google.com" class="font-medium text-purple-600">Visit the Trust Center <span class="text-2xl">→</span>
+      </a>
     </div>
     <div class="border-3 border-red-900 grid grid-cols-2 ">
-      <div class="border">
+      <div class="border flex flex-col gap-2">
         <div><img src="/server.png" alt="server icon" class="w-10 h-10"></div>
         <h2 class="font-bold text-2xl">99.99% uptime</h2>
         <p class="font-light text-gray-500">For Landwind, with zero maintenance downtime</p>
       </div>
-      <div class="border">
+      <div class="border flex flex-col gap-2">
         <div><img src="/persons.png" alt="two people icon" class="w-10 h-10"></div>
         <h2 class="font-bold text-2xl">600M+ Users</h2>
         <p class="font-light text-gray-500">Trusted by over 600 milion users around the world</p>
       </div>
-      <div class="border">
+      <div class="border flex flex-col gap-2">
         <div><img src="/earth.png" alt="earth logo" class="w-10 h-10"></div>
         <h2 class="font-bold text-2xl">100+ countries</h2>
         <p class="font-light text-gray-500">Have used Landwind to create functional websites</p>
       </div>
-      <div class="border">
+      <div class="border flex flex-col gap-2">
         <div><img src="/cart.png" alt="shopping cart" class="w-10 h-10"></div>
         <h2 class="font-bold text-2xl">5+ Million</h2>
-        <p class="font-light text-gray-500">FTransactions per day</p>
+        <p class="font-light text-gray-500">Transactions per day</p>
       </div>
     </div>
   </section>
 
 
-  <section id="fifth-section" class="border bg-[#F9FAFB]">
-    <div><img src="/PhQuotesFill (1).png" alt="quotes" class="w-10 m-auto"></div>
-    <p class="font-medium text-2xl text-gray-900">"Landwind is just awesome. It contains tons of predesigned components
-      and pages starting from login screen to
-      complex dashboard. Perfect choice for your next SaaS application."</p>
-    <div class="flex justify-center items-center border ">
-      <div><img src="/Favicon.png" alt="image of a girl" class="w-7"></div>
-      <p>Ballnerina Cappuchina</p>
-      <p>CEO at Google</p>
-    </div>
+  <section id="fifth-section" class="border-4 border-green-900 bg-[#F9FAFB] p-[96px] flex flex-col gap-6">
+      <div><img src="/PhQuotesFill (1).png" alt="quotes" class="w-10 m-auto"></div>
+      <p class="font-medium text-2xl text-gray-900">"Landwind is just awesome. It contains tons of predesigned
+        components
+        and pages starting from login screen to
+        complex dashboard. Perfect choice for your next SaaS application."</p>
+      <div class="flex justify-center items-center gap-2">
+        <div><img src="/Favicon.png" alt="image of a girl" class="w-7"></div>
+        <p class="font-medium">Ballnerina Cappuchina</p>
+        <p class="font-light text-gray-400 border-l-2 border-gray-900 pl-2">CEO at Google</p>
+      </div>
   </section>
 </template>
 
