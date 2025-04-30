ada79772c6fc961726a19ec8d481457de435ec70
Shii108 <sujalpoudel01@gmail.com>
2025-04-30 07:29:29 +0545

    feat: responsiveness of second section

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,9 +1,12 @@
 <template>
   <section id="hero-section" class="flex border p-4">
     <div class="border border-red-900 flex flex-col gap-6 justify-center">
-      <h1 class="text-[clamp(3rem,2.786rem+1.07vw,3.75rem)] font-extrabold tracking-loose lg:tracking-tight leading-none text-gray-900">Building digital <br>
+      <h1
+        class="text-[clamp(3rem,2.786rem+1.07vw,3.75rem)] font-extrabold tracking-loose lg:tracking-tight leading-none text-gray-900">
+        Building digital <br>
         products & brands.</h1>
-      <p class="lg:text-2xl text-xl text-gray-500 font-light">This free and open-source landing page template was built using the
+      <p class="lg:text-2xl text-xl text-gray-500 font-light">This free and open-source landing page template was built
+        using the
         utility classes from Tailwind CSS and based
         on the components from the Flowbite Library and the Blocks System.</p>
       <div class="flex flex-col lg:flex-row gap-6">
@@ -21,8 +24,8 @@
   </section>
 
 
-  <section id="second-section" class="border p-4">
-    <div class="lg:flex lg:justify-between border py-8">
+  <section id="popular-company" class="border p-4">
+    <div class="grid grid-cols-2 gap-6 font-medium  md:grid-cols-3 md:gap-8 lg:flex lg:justify-between border py-8">
       <h2 class="text-3xl text-gray-400  hover:text-gray-900">airbnb</h2>
       <h2 class="text-3xl text-gray-400  hover:text-gray-900">Google</h2>
       <h2 class="text-3xl text-gray-400  hover:text-gray-900">Microsoft</h2>
@@ -33,7 +36,7 @@
   </section>
 
 
-  <section id="third-section" class="bg-[#F9FAFB] py-4">
+  <section id="third-section" class="bg-[#F9FAFB] border p-4">
     <div class="flex border-3 border-green-800 py-[69px]">
       <div class="flex flex-col py-4 border justify-between">
         <h2 class="text-3xl font-extrabold text-gray-900 tracking-wide">Work with tools you already use</h2>
@@ -99,57 +102,59 @@
   </section>
 
 
-  <section id="fourth-section" class="flex md:flex-row flex-col border py-[96px]">
-    <div class="border-3 border-purple-900 flex-1/2 flex flex-col gap-4">
-      <p class="font-medium text-lg text-purple-600">Trusted Worldwide</p>
-      <h2 class="text-3xl font-extrabold text-gray-900">Trusted by over 600 million users and 10,000 teams</h2>
-      <p class="text-gray-500 font-light text-xl border-t pt-4">Our rigorous security and compliance standards are at
-        the heart of
-        all we do. We work tirelessly to protect you
-        and your customers.</p>
-      <a href="www.google.com" class="font-medium text-purple-600 block">Explore Legality Guide <span
-          class="text-2xl">→</span></a>
-      <a href="www.google.com" class="font-medium text-purple-600">Visit the Trust Center <span
-          class="text-2xl">→</span>
-      </a>
-    </div>
-    <div class="border-3 border-red-900 grid md:grid-cols-2 ">
-      <div class="border flex flex-col gap-2">
-        <div><img src="/server.png" alt="server icon" class="w-10 h-10"></div>
-        <h2 class="font-bold text-2xl">99.99% uptime</h2>
-        <p class="font-light text-gray-500">For Landwind, with zero maintenance downtime</p>
+  <section id="unique-features" class="flex md:flex-row flex-col border p-4">
+      <div class="border-3 border-purple-900 flex-1/2 flex flex-col gap-4">
+        <p class="font-medium text-lg text-purple-600">Trusted Worldwide</p>
+        <h2 class="text-3xl font-extrabold text-gray-900">Trusted by over 600 million users and 10,000 teams</h2>
+        <p class="text-gray-500 font-light text-xl border-t pt-4">Our rigorous security and compliance standards are at
+          the heart of
+          all we do. We work tirelessly to protect you
+          and your customers.</p>
+        <a href="www.google.com" class="font-medium text-purple-600 block">Explore Legality Guide <span
+            class="text-2xl">→</span></a>
+        <a href="www.google.com" class="font-medium text-purple-600">Visit the Trust Center <span
+            class="text-2xl">→</span>
+        </a>
       </div>
-      <div class="border flex flex-col gap-2">
-        <div><img src="/persons.png" alt="two people icon" class="w-10 h-10"></div>
-        <h2 class="font-bold text-2xl">600M+ Users</h2>
-        <p class="font-light text-gray-500">Trusted by over 600 milion users around the world</p>
+      <div class="border-3 border-red-900 grid md:grid-cols-2 ">
+        <div class="border flex flex-col gap-2">
+          <div><img src="/server.png" alt="server icon" class="w-10 h-10"></div>
+          <h2 class="font-bold text-2xl">99.99% uptime</h2>
+          <p class="font-light text-gray-500">For Landwind, with zero maintenance downtime</p>
+        </div>
+        <div class="border flex flex-col gap-2">
+          <div><img src="/persons.png" alt="two people icon" class="w-10 h-10"></div>
+          <h2 class="font-bold text-2xl">600M+ Users</h2>
+          <p class="font-light text-gray-500">Trusted by over 600 milion users around the world</p>
+        </div>
+        <div class="border flex flex-col gap-2">
+          <div><img src="/earth.png" alt="earth logo" class="w-10 h-10"></div>
+          <h2 class="font-bold text-2xl">100+ countries</h2>
+          <p class="font-light text-gray-500">Have used Landwind to create functional websites</p>
+        </div>
+        <div class="border flex flex-col gap-2">
+          <div><img src="/cart.png" alt="shopping cart" class="w-10 h-10"></div>
+          <h2 class="font-bold text-2xl">5+ Million</h2>
+          <p class="font-light text-gray-500">Transactions per day</p>
+        </div>
       </div>
-      <div class="border flex flex-col gap-2">
-        <div><img src="/earth.png" alt="earth logo" class="w-10 h-10"></div>
-        <h2 class="font-bold text-2xl">100+ countries</h2>
-        <p class="font-light text-gray-500">Have used Landwind to create functional websites</p>
-      </div>
-      <div class="border flex flex-col gap-2">
-        <div><img src="/cart.png" alt="shopping cart" class="w-10 h-10"></div>
-        <h2 class="font-bold text-2xl">5+ Million</h2>
-        <p class="font-light text-gray-500">Transactions per day</p>
-      </div>
-    </div>
   </section>
 
 
-  <section id="fifth-section" class="border-4 border-green-900 bg-[#F9FAFB] p-[96px] flex flex-col gap-6">
+  <section id="quotes" class="border p-4">
+    <div class="border border-green-900 bg-[#F9FAFB] flex flex-col gap-6">
     <div><img src="/PhQuotesFill (1).png" alt="quotes" class="w-10 m-auto"></div>
-    <p class="font-medium text-2xl text-gray-900">"Landwind is just awesome. It contains tons of predesigned
+    <p class="font-medium text-[clamp(1.25rem,1.179rem+0.36vw,1.5rem)] text-gray-900">"Landwind is just awesome. It contains tons of predesigned
       components
       and pages starting from login screen to
       complex dashboard. Perfect choice for your next SaaS application."</p>
-    <div class="flex justify-center items-center gap-2">
-      <div><img src="/Favicon.png" alt="image of a girl" class="w-7"></div>
-      <p class="font-medium">Ballnerina Cappuchina</p>
-      <p class="font-light text-gray-400 border-l-2 border-gray-900 pl-2">CEO at Google</p>
-    </div>
-  </section>
+      <div class="flex justify-center items-center gap-2">
+        <div><img src="/Favicon.png" alt="image of a girl" class="w-7"></div>
+        <p class="font-medium">Ballnerina Cappuchina</p>
+        <p class="font-light text-gray-400 border-l-2 border-gray-900 pl-2">CEO at Google</p>
+      </div>
+      </div>
+    </section>
 </template>
 
 <script setup>
