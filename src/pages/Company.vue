8218203ce041905110d6065da2623ae52b6f9a3c
Shii108 <sujalpoudel01@gmail.com>
2025-05-15 18:14:38 +0545

    i amd done with this sayonnara

--- src/pages/Company.vue
+++ src/pages/Company.vue
@@ -1,17 +1,12 @@
 <template>
-  <div>
-    This is just for testing purpose.
+  <div class="h-[90vh] flex justify-center items-center">
+    <img src="/mimi.png" alt="" width="200px" class="border-gray-600 rounded-md">
   </div>
 </template>
 
 <script setup>
 
 
-
-
-
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
